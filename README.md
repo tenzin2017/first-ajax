01c62b261efd1dd2076869b5d9b6025d217367c3
tenzin2017 <tenzinleo@gmail.com>
2017-10-03 17:09:56 -0400

    my first ajax assignment

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+STEP0:
+  1. GET method
+  2. 42ms
+  3. 200, OK or successful
+  4. Initiator: other, Type:       
+      document
+
+STEP1:
+  5. method: GET , Status: 200
+  6. Type: xhr, Initiator: ajax.js
+
+STEP2:
--- ajax.js
+++ ajax.js
@@ -1,5 +1,79 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var button1 = document.querySelector('.step12');
+  var button2 = document.querySelector('.step3456');
+  var button3 = document.querySelector('.step7');
+  var button4 = document.querySelector('.step8');
+  var button5 = document.querySelector('.step9');
+  var section = document.querySelector('#step3456');
+  var sectionLast = document.querySelector('#step9');
+  var ul = document.querySelector('#car');
 
-  /* Your code goes here */
+  button1.addEventListener('click', function(){
+    $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/',
+    method: 'GET',
+    data: '',
+    dataType:'txt'
+  });
+  });
+
+  button2.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/pong',
+      method: 'GET',
+      data: '',
+      dataType: 'html'
+    }).done(function(responseData){
+      console.log('We did it');
+      var p = document.createElement('p');
+      p.innerText = responseData;
+      section.appendChild(p);
+    }).fail(function(){
+      console.log("That didn't go well");
+
+    }).always(function(){
+      console.log("All I know is , it's over !");
+    });
+  });
+
+  button3.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      data: '',
+      dataType: 'html'
+    }).done(function(responseData) {
+      var p = document.createElement('p');
+      p.innerText = 'Number of visitors: '+responseData
+      sectionLast.append(p);
+      console.log(responseData);
+    });
+  });
+
+  button4.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data: {timezone: 'Asia/Kolkata'},
+      dataType: 'html'
+    }).done(function(responseData){
+      var p = document.createElement('p');
+      p.innerText = 'Current TimeZone : '+responseData;
+      sectionLast.appendChild(p);
+    });
+  });
+
+  button5.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      method: 'GET',
+      data: '',
+      dataType: 'html'
+    }).done(function(responseData) {
+      var li = document.createElement('li');
+      li.innerHTML= responseData;
+      ul.appendChild(li);
+    });
+  });
 
 });
